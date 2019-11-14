9f61bb2e9a18e7bf0cb089a8b21d03568ae76867
dragon <809629457@qq.com>
2019-11-14 21:37:40 +0800

    提交

---
+++ file.c
@@ -0,0 +1,31 @@
+//
+// Created by hasee on 2019/9/23.
+//
+
+#include <stdio.h>
+
+int main(){
+    FILE *pr = fopen("./a.txt", "r");
+    if(pr){
+        while(!feof(pr)){
+            char c = fgetc(pr);
+            printf("i=%s\n",c);
+        }
+    } else{
+        printf("r³ö´í");
+    }
+    fclose(pr);
+    
+    FILE *pw = fopen("./a.txt", "w");
+    if (pw){
+        fputc('a', pw);
+    } else{
+        printf("w³ö´í");
+    }
+    fclose(pw);
+
+
+
+
+
+}
